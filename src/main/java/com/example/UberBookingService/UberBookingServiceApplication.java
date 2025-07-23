f700834eabc00d2ba5422c704e2e4fc270983c3d
Pratham-Anand <anandpratham13@gmail.com>
2025-07-23 20:15:55 +0530

    configiured eureka client and downgraded spring boot version due to conflicts with spring cloud

--- src/main/java/com/example/UberBookingService/UberBookingServiceApplication.java
+++ src/main/java/com/example/UberBookingService/UberBookingServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.UberBookingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.example.UberEntityService.models"})
+@EntityScan("com.example.UberEntityService.models")
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class UberBookingServiceApplication {
 
 	public static void main(String[] args) {
